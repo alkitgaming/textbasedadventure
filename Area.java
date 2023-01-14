f5f345baed44fa42260c33336d883d9975a852b3
alkitgaming <alkitgaming@gmail.com>
2023-01-13 19:14:01 -0600

    Added generateBoard method to main, constructors and instance variables for Area class, and adder methods for the ArrayList instance variables in Area class

--- Area.java
+++ Area.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
 public class Area 
 {
+    private ArrayList<Enemy> enemies;
+    private ArrayList<String> items;
+    private String description;
+    private String name;
+
+    public Area(String name, String description)
+    {
+        this.name = name;
+        this.description = description;
+    }
     
+    public Area()
+    {
+        this.name = "";
+        this.description = "";
+    }
+
+    public void addEnemy(Enemy e)
+    {
+        enemies.add(e);
+    }
+
+    public void addItem(String i)
+    {
+        items.add(i);
+    }
 }
--- TextGame.java
+++ TextGame.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
 public class TextGame 
 {
     public static void main(String[] args)
     {
         
+    }
+
+    public static Area[][] generateBoard(int x, int y, ArrayList<Enemy> enemies, ArrayList<String> items)
+    {
+        Area[][] board = new Area[x][y];
+        for (Enemy e : enemies)
+        {
+            int randomX = (int) Math.random() * x + 1;
+            int randomY = (int) Math.random() * y + 1;
+            board[randomX][randomY].addEnemy(e);
+        }
+        for (String i : items)
+        {
+            int randomX = (int) Math.random() * x + 1;
+            int randomY = (int) Math.random() * y + 1;
+            board[randomX][randomY].addItem(i);
+        }
+        return board;
     }
 }
